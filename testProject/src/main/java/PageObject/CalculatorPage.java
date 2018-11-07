24b801633587ee1af68b3a00c37dac7800a04f49
DESKTOP-AVQ3BDH\Alex <alexander@playbuzz.com>
2018-11-07 02:58:02 +0200

    WIP

--- testProject/src/main/java/PageObject/CalculatorPage.java
+++ testProject/src/main/java/PageObject/CalculatorPage.java
@@ -1,8 +1,13 @@
 package PageObject;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import enums.CalculatorOperator;
 
@@ -10,57 +15,89 @@ public class CalculatorPage {
 	private WebDriver driver;
 	private final String PREFIX = "Btn";
 	private final String CALC = "Calc";
-	private final String DOT = "Dot";
-	private final String SIGN = "Sign";
 	private final By INPUT = By.id("input");
+	private final By COOKIES = By.name("cookies");
+	private final By LOADING = By.className("loading");
+	private final String URL = "https://web2.0calc.com/";
+	private final String CLEAR = "Clear";
+	private WebDriverWait wait;
 	
 	public CalculatorPage(WebDriver driver) {
 		this.driver = driver;
+		wait = new WebDriverWait(driver, 30000);
+		this.driver.navigate().to(URL);
+		if(!driver.findElements(COOKIES).isEmpty()){
+			wait.until(ExpectedConditions.elementToBeClickable(COOKIES));
+		}
+	    driver.findElement(COOKIES).click();
 	}
 
-	public String getBinaryResult(double firstParameter, double secondParameter, CalculatorOperator operator) throws Exception {
-		insertDoubleNumber(firstParameter);
+	public void clearResult() {
+		typeFromKey(CLEAR);
+	}
+
+	public String getTrigoResult(String input, CalculatorOperator operator) throws Exception {
+		insertOperator(operator);
+		insertFormula(input);
+		return  calculateAndGetResult();
+	}
+
+	public String getCalculationResult(String formula) throws Exception {
+		insertFormula(formula);
+		return  calculateAndGetResult();
+	}
+
+	
+	
+	private void insertOperator(CalculatorOperator operator) {
 		typeFromKey(operator.getValue());
-		insertDoubleNumber(secondParameter);
+	}
+
+
+	private String calculateAndGetResult() {
 		calculate();
 		return getResult();
 	}
-
-	private String getResult() {
-	return driver.findElement(INPUT).getAttribute("value");
-	}
-
-	private void calculate() {
-		typeFromKey(CALC);
-	}
-
-	private void insertDoubleNumber(double number) throws Exception {
-		String numberStr = Double.toString(number);
-		char[] numberArray = numberStr.toCharArray();
-		for(char charInput : numberArray) {
-			if(Character.isDigit(charInput))
-				typeFromKey(Character.toString(charInput));
-			else if (charInput == '.')
-				typeFromKey(DOT);
-			else if (charInput == '-')
-				typeFromKey(SIGN);
-			else
-				throw new Exception("invalid input");
-		}
-
-		}
 
 	private void typeFromKey(String input) {
 		String id = String.format("%s%s",PREFIX, input);
+		wait.until(ExpectedConditions.elementToBeClickable(By.id(id)));
 		WebElement inputElement = driver.findElement(By.id(id));
 		inputElement.click();
 	}
+	
+	private String getResult() {
+		return driver.findElement(INPUT).getAttribute("value");
+		}
 
-	public String getTrigoResult(double input, CalculatorOperator operator) throws Exception {
-		typeFromKey(operator.getValue());
-		insertDoubleNumber(input);
-		calculate();
-		return getResult();
-	}	
+	private void calculate() {
+			typeFromKey(CALC);
+			wait.until(ExpectedConditions.numberOfElementsToBe(LOADING, 0));
+		}
 
+		private void insertFormula (String formula) throws Exception {
+			char[] formulaArray = formula.toCharArray();
+			for(char charInput : formulaArray) {
+				if(getSpecialCharacters().containsKey(charInput))
+					typeFromKey(getSpecialCharacters().get(charInput));
+				else if(Character.isDigit(charInput))
+					typeFromKey(Character.toString(charInput));
+				else
+					throw new Exception("invalid input");
+				}
+				
+			}
+		
+		private static Map<Character, String> getSpecialCharacters()
+		{
+		    Map<Character, String> specialCharacters = new HashMap<Character, String>();
+			specialCharacters.put('.', "Dot");
+			specialCharacters.put('-', "Sign");
+			specialCharacters.put('(', "ParanL");
+			specialCharacters.put(')', "ParanR");		
+			specialCharacters.put('-', "Minus");
+			specialCharacters.put('+', "Plus");
+			specialCharacters.put('*', "Mult");
+			return specialCharacters;
+		}
 }
--- testProject/src/main/java/UiTests/CalculatorTests.java
+++ testProject/src/main/java/UiTests/CalculatorTests.java
@@ -1,62 +1,84 @@
 package UiTests;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import PageObject.CalculatorPage;
-import enums.CalculatorOperator;
 
 
 public class CalculatorTests {
 	
-	private WebDriver driver;
+	private static WebDriver driver;
+	private static CalculatorPage calculator;
 
+	
+	@BeforeClass
+	public static void classSetUp() throws Exception {
+	   driver = new ChromeDriver();
+	   calculator = new CalculatorPage(driver);
+	}
+	
+	
 	@Before
 	public void setUp() throws Exception {
-		driver = new ChromeDriver();
+		calculator.clearResult();
 	}
 
+
+	String formulaAdd = "2+3";
+	String formulaSubstraction = "10-2";
+	String formulaComplex = "(10-2)*2";
+	String formulaTrigo = "sin(30)";
+	
+	String expectedResultAdd = "5";
+	String expectedResultSubstraction = "8";
+	String notValidResultComplex = "20";
+	String expectedResultTrigo = "0.5";
+	
 
 	@Test
 	public void testAddition() throws Exception {
-		String expectedResult = "5";
-		CalculatorPage calculator = new CalculatorPage(driver);
-		String result = calculator.getBinaryResult(2, 3, CalculatorOperator.ADDITION);
-		Assert.assertEquals(expectedResult, result);
+		String result = calculator.getCalculationResult(formulaAdd);
+		Assert.assertEquals(expectedResultTrigo, result);
 	}
 	
 	@Test
-	public void testSubtraction() throws Exception {
-		String expectedResult = "8";
-		CalculatorPage calculator = new CalculatorPage(driver);
-		String result = calculator.getBinaryResult(10, 2, CalculatorOperator.SUBTRACTION);
-		Assert.assertEquals(expectedResult, result);
+	public void testSubtraction() throws Exception  {
+		String result = calculator.getCalculationResult(formulaSubstraction);
+		Assert.assertEquals(expectedResultSubstraction, result);
 	}
 	
 	@Test
-	public void testComplex() throws NumberFormatException, Exception {
-		String notValidResult = "20";
-		CalculatorPage calculator = new CalculatorPage(driver);
-		String substractionResult = calculator.getBinaryResult(10, 2, CalculatorOperator.SUBTRACTION);		
-		String result = calculator.getBinaryResult(Double.parseDouble(substractionResult), 2, CalculatorOperator.MULTIPLICATION);
-		Assert.assertNotEquals(notValidResult, result);
+	public void testComplex() throws Exception {
+		String result = calculator.getCalculationResult(formulaComplex);
+		Assert.assertNotEquals(notValidResultComplex, result);
 	}
 	
 	@Test
 	public void testSinus() throws Exception {
-		String expectedResult = "0.5";
-		CalculatorPage calculator = new CalculatorPage(driver);
-		String result = calculator.getTrigoResult(30, CalculatorOperator.SINUS);
-		Assert.assertEquals(expectedResult, result);
+		String result = calculator.getCalculationResult(formulaTrigo);
+		Assert.assertEquals(expectedResultTrigo, result);
+	}
+	
+	@Test
+	public void testHistory() {
+		
 	}
 	
 	
 	@After
 	public void tearDown() throws Exception {
+		calculator.clearResult();
+	}
+	
+	@AfterClass
+	public static void classTearDown() throws Exception {
 		driver.close();
 	}
 
